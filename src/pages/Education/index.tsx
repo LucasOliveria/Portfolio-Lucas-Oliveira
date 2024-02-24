bf50ce92e302bd87eec4d2fedfba10197540a86f
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2024-02-23 21:45:59 -0300

    format margin-bottom

--- src/pages/Education/index.tsx
+++ src/pages/Education/index.tsx
@@ -3,7 +3,7 @@ import ContainerRight from '../../components/ContainerRight'
 import './style.css'
 import screenTemplateEducation from "../../assets/screenTemplates/screen-template-education.svg";
 import cyberpunkShip1 from "../../assets/ships/cyberpunk_ship1.svg";
-// import cyberpunkShip2 from "../../assets/ships/cyberpunk_ship2.svg";
+import cyberpunkShip2 from "../../assets/ships/cyberpunk_ship2.svg";
 
 function Education() {
   return (
@@ -25,6 +25,16 @@ function Education() {
               <p>JavaScript, PHP, Python, Java, POO Estrutura de dados, Desenvolvimento web</p>
             </div>
             <img src={cyberpunkShip1} alt="cyberpunk-ship" />
+          </div>
+          <div className="education-box">
+            <img src={screenTemplateEducation} alt="screen-template-education" />
+            <div className="education-text">
+              <p><span>Análise e Desenvolvimento de Sistemas (EAD) - Faculdade Estácio
+                (01/2024 - 03/2026)</span></p>
+              <p>Implementação de projetos, documentação, especificação, testes, implementação e manutenção de sistemas computacionais e software.</p>
+              <p>JavaScript, PHP, Python, Java, POO Estrutura de dados, Desenvolvimento web</p>
+            </div>
+            <img src={cyberpunkShip2} alt="cyberpunk-ship" />
           </div>
         </div>
       </ContainerRight>
