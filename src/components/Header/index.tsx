438513040a51bca97312396e5f3859ee850f841e
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2024-03-13 20:53:02 -0300

    Add new assets; Add menu mobile; Add new entrace animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,13 +6,19 @@ import githubIcon from "../../assets/social/github.svg"
 import linkedinIcon from "../../assets/social/linkedin.svg"
 import useStatesContext from "../../hooks/useStatesContext"
 import './style.css'
+import buttonOpen from "../../assets/menuButtons/open-menu-icon.svg"
+import buttonClose from "../../assets/menuButtons/close-menu-icon.svg"
+import { useState } from "react"
 
 function Header() {
   const location = useLocation();
   const navigate = useNavigate();
+  const [openMenu, setOpenMenu] = useState(false);
   const { setExit } = useStatesContext();
 
   function handleNavigatAnimate(page: string) {
+    setOpenMenu(false);
+
     if (page !== location.pathname) {
       if (location.pathname === "/home") {
         setExit("home");
@@ -63,6 +69,10 @@ function Header() {
     }
   }
 
+  function handleOpenMenu() {
+    setOpenMenu(!openMenu);
+  }
+
   return (
     <header>
       <NavLink to={"/home"}>
@@ -71,84 +81,92 @@ function Header() {
           <h3>#LucasOlivr07</h3>
         </div>
       </NavLink>
-      <nav className='menu'>
-        <ul className='menu-content'>
-          <li>
-            <img
-              className={`${location.pathname === "/home" ? "img-visible" : "img-hidden"}`}
-              src={currentFolderIcon}
-              alt="folder-icon"
-            />
-            <p
-              onClick={() => handleNavigatAnimate("/home")}
-              className={`${location.pathname === "/home" && "p-active"}`}
-            >
-              Sobre mim
-            </p>
-          </li>
-          <li>
-            <img
-              className={`${location.pathname === "/education" ? "img-visible" : "img-hidden"}`}
-              src={currentFolderIcon}
-              alt="folder-icon"
-            />
-            <p
-              onClick={() => handleNavigatAnimate("/education")}
-              className={`${location.pathname === "/education" && "p-active"}`}
-            >
-              Formação
-            </p>
-          </li>
-          <li>
-            <img
-              className={`${location.pathname === "/skills" ? "img-visible" : "img-hidden"}`}
-              src={currentFolderIcon}
-              alt="folder-icon"
-            />
-            <p
-              onClick={() => handleNavigatAnimate("/skills")}
-              className={`${location.pathname === "/skills" && "p-active"}`}
-            >
-              Habilidades
-            </p>
-          </li>
-          <li>
-            <img
-              className={`${location.pathname === "/projects" ? "img-visible" : "img-hidden"}`}
-              src={currentFolderIcon}
-              alt="folder-icon"
-            />
-            <p
-              onClick={() => handleNavigatAnimate("/projects")}
-              className={`${location.pathname === "/projects" && "p-active"}`}
-            >
-              Projetos
-            </p>
-          </li>
-          <li>
-            <img
-              className={`${location.pathname === "/contact" ? "img-visible" : "img-hidden"}`}
-              src={currentFolderIcon}
-              alt="folder-icon"
-            />
-            <p
-              onClick={() => handleNavigatAnimate("/contact")}
-              className={`${location.pathname === "/contact" && "p-active"}`}
-            >
-              Contato
-            </p>
-          </li>
-        </ul>
-        <div className='social-menu'>
-          <a href="mailto:lucasoliveiralog@hotmail.com">
-            <img src={emailIcon} alt="email-icon" />
-          </a>
-          <a href="http://www.linkedin.com/in/lucas-oliveira-5b8a5532" target='_blank' >
-            <img src={linkedinIcon} alt="linkedin-icon" />
-          </a>
-          <a href="http://github.com/LucasOliveria" target="_blank">
-            <img src={githubIcon} alt="gitbub-icon" />
-          </a>
+      <nav className={`menu ${openMenu && "menu-open fade-in"}`}>
+        {
+          !openMenu ?
+            <img src={buttonOpen} alt="open button" onClick={handleOpenMenu} />
+            :
+            <img src={buttonClose} alt="close button" onClick={handleOpenMenu} />
+        }
+        <div className="container-nav-menus">
+          <ul className='menu-content'>
+            <li>
+              <img
+                className={`${location.pathname === "/home" ? "img-visible" : "img-hidden"}`}
+                src={currentFolderIcon}
+                alt="folder-icon"
+              />
+              <p
+                onClick={() => handleNavigatAnimate("/home")}
+                className={`${location.pathname === "/home" && "p-active"}`}
+              >
+                Sobre mim
+              </p>
+            </li>
+            <li>
+              <img
+                className={`${location.pathname === "/education" ? "img-visible" : "img-hidden"}`}
+                src={currentFolderIcon}
+                alt="folder-icon"
+              />
+              <p
+                onClick={() => handleNavigatAnimate("/education")}
+                className={`${location.pathname === "/education" && "p-active"}`}
+              >
+                Formação
+              </p>
+            </li>
+            <li>
+              <img
+                className={`${location.pathname === "/skills" ? "img-visible" : "img-hidden"}`}
+                src={currentFolderIcon}
+                alt="folder-icon"
+              />
+              <p
+                onClick={() => handleNavigatAnimate("/skills")}
+                className={`${location.pathname === "/skills" && "p-active"}`}
+              >
+                Habilidades
+              </p>
+            </li>
+            <li>
+              <img
+                className={`${location.pathname === "/projects" ? "img-visible" : "img-hidden"}`}
+                src={currentFolderIcon}
+                alt="folder-icon"
+              />
+              <p
+                onClick={() => handleNavigatAnimate("/projects")}
+                className={`${location.pathname === "/projects" && "p-active"}`}
+              >
+                Projetos
+              </p>
+            </li>
+            <li>
+              <img
+                className={`${location.pathname === "/contact" ? "img-visible" : "img-hidden"}`}
+                src={currentFolderIcon}
+                alt="folder-icon"
+              />
+              <p
+                onClick={() => handleNavigatAnimate("/contact")}
+                className={`${location.pathname === "/contact" && "p-active"}`}
+              >
+                Contato
+              </p>
+            </li>
+          </ul>
+          <div className='social-menu'>
+            <a href="mailto:lucasoliveiralog@hotmail.com">
+              <img src={emailIcon} alt="email-icon" />
+            </a>
+            <a href="http://www.linkedin.com/in/lucas-oliveira-5b8a5532" target='_blank' >
+              <img src={linkedinIcon} alt="linkedin-icon" />
+            </a>
+            <a href="http://github.com/LucasOliveria" target="_blank">
+              <img src={githubIcon} alt="gitbub-icon" />
+            </a>
+          </div>
         </div>
       </nav>
     </header>
